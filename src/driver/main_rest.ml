0a23654e6291f0db7c425fa9924b6472465e55f9
zoggy <zoggy@bat8.org>
2012-03-05 18:36:23 +0100

    skeleton of rest web api, using ocamlnet

--- src/driver/main_rest.ml
+++ src/driver/main_rest.ml
@@ -1,5 +1,62 @@
 (** Main module of the REST web API. *)
 
+
+let rest_api rdf_wld host port (cgi : Netcgi.cgi_activation) =
+ let env = cgi#environment in
+ prerr_endline ("#cgi_server_name=" ^ env#cgi_server_name);
+ prerr_endline ("#cgi_server_protocol=" ^ env#cgi_server_protocol);
+ prerr_endline ("#cgi_path_info=" ^ env#cgi_path_info);
+ prerr_endline ("#cgi_path_translated=" ^ env#cgi_path_translated);
+ prerr_endline ("#cgi_query_string=" ^ env#cgi_query_string);
+ let o = cgi#out_channel in
+
+  (* Set the header. The header specifies that the page must not be
+   * cached. This is important for dynamic pages called by the GET
+   * method, otherwise the browser might display an old version of
+     * the page.*)
+  cgi#set_header
+  ~cache:`No_cache
+  ~content_type:"application/json; charset=\"utf-8\""
+  ();
+
+  o#output_string "coucou"
+;;
+
+let process handler (cgi : Netcgi.cgi_activation) =
+  (* The [try] block catches errors during the page generation. *)
+  try
+
+    handler cgi;
+
+    (* After the page has been fully generated, we can send it to the
+     * browser.
+     *)
+    cgi#out_channel#commit_work();
+  with
+      error ->
+        (* An error has happened. Generate now an error page instead of
+         * the current page. By rolling back the output buffer, any
+         * uncomitted material is deleted.
+         *)
+        cgi#output#rollback_work();
+
+        (* We change the header here only to demonstrate that this is
+         * possible.
+         *)
+        cgi#set_header
+          ~status:`Forbidden                  (* Indicate the error *)
+          ~cache:`No_cache
+          ~content_type:"test/ascii; charset=\"iso-8859-1\""
+          ();
+
+        cgi#output#output_string "While processing the request an O'Caml exception has been raised:\n";
+        cgi#output#output_string ((Printexc.to_string error) ^ "\n");
+
+        (* Now commit the error page: *)
+        cgi#output#commit_work()
+;;
+
+
 let options =
   Options.option_version "Genet-rest" ::
   Options.option_config ::
@@ -7,11 +64,93 @@ let options =
   []
 ;;
 
+let config_tree host port =
+  `Section ("netplex",
+   [
+     `Section ("service",
+      [
+        `Parameter ("name", `String "nethttpd") ;
+        `Section ("protocol",
+         [
+           `Parameter ("name", `String "http") ;
+           `Section ("address",
+            [ `Parameter ("type", `String "internet") ;
+              `Parameter ("bind", `String (Printf.sprintf "0.0.0.0:%d" port));
+            ]) ;
+         ]) ;
+        `Section ("processor",
+         [
+           `Parameter ("type", `String "nethttpd") ;
+           `Parameter ("access_log", `String "debug");  (* or "off" or "enabled" *)
+           `Parameter ("suppress_broken_pipe", `Bool true);
+           `Section ("host",
+            [
+              (* Think of Apache's "virtual hosts" *)
+              `Parameter ("pref_name", `String host) ;
+              `Parameter ("pref_port", `Int port);
+              `Parameter ("names", `String "*:0"); (* Which requests are matched here: all *)
+              `Section ("uri",
+               [
+                 `Parameter ("path", `String "/");
+                 `Section ("service",
+                  [ `Parameter ("type", `String "dynamic") ;
+                    `Parameter ("handler", `String "api");
+                  ]);
+               ]);
+            ]);
+         ]);
+        `Section ("workload_manager",
+         [
+           `Parameter ("type", `String "dynamic");
+           `Parameter ("max_jobs_per_thread", `Int 1);  (* Everything else is senseless *)
+           `Parameter ("min_free_jobs_capacity", `Int 1);
+           `Parameter ("max_free_jobs_capacity", `Int 1);
+           `Parameter ("max_threads", `Int 20);
+         ]);
+      ]);
+   ]
+  )
+;;
+
+
 let main () =
   let opts = Options.parse options in
   let config = Config.read_config opts.Options.config_file in
+  let url = Neturl.parse_url config.Config.rest_api in
+  let host = Neturl.url_host url in
+  let port = Neturl.url_port url in
+  let path = Neturl.join_path (Neturl.url_path url) in
+
+  if opts.Options.verb_level > 0 then
+    prerr_endline (Printf.sprintf "host=%s, port=%d, path=%s" host port path);
   let rdf_wld = Grdf_init.open_storage config in
-  ignore(rdf_wld)
+
+  let parallelizer =
+    (*Netplex_mt.mt()*)     (* multi-threading *)
+    Netplex_mp.mp()   (* multi-processing *)
+  in
+  let api =
+    { Nethttpd_services.dyn_handler = (fun _ -> process (rest_api rdf_wld host port));
+      dyn_activation = Nethttpd_services.std_activation `Std_activation_buffered;
+      dyn_uri = Some "/";                 (* not needed *)
+      dyn_translator = (fun s -> s); (* not needed *)
+      dyn_accept_all_conditionals = true ;
+    }
+  in
+  let nethttpd_factory =
+    Nethttpd_plex.nethttpd_factory
+    ~handlers:[ "api", api ]
+    ()
+  in
+  let config_tree = config_tree host port in
+  let netplex_config = Netplex_main.create ~config_tree () in
+  let netplex_config = Netplex_main.modify ~foreground: true netplex_config in
+  Netplex_main.startup
+    parallelizer
+    Netplex_log.logger_factories   (* allow all built-in logging styles *)
+    Netplex_workload.workload_manager_factories (* ... all ways of workload management *)
+    [ nethttpd_factory ]           (* make this nethttpd available *)
+    netplex_config
 ;;
 
 let () = Misc.safe_main main;;
