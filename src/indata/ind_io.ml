09d52a59f589f32b83009ed3c07831cc7e2e48bc
zoggy <zoggy@bat8.org>
2012-05-23 08:21:00 +0200

    use config_file to load input spec

---
+++ src/indata/ind_io.ml
@@ -0,0 +1,58 @@
+(** *)
+
+module CF = Config_file;;
+open Ind_types;;
+
+let input_basename = "spec.in";;
+
+exception Error of string * string (** file * msg *)
+
+let error ~file msg = raise (Error (file, msg));;
+
+type input =
+{ group : CF.group ;
+  in_cp : string list CF.cp ;
+  out_cp : string list CF.cp ;
+  chains_cp : string list CF.cp ;
+}
+
+let mk_spec_group () =
+  let group = new CF.group in
+  let in_cp = new CF.list_cp CF.string_wrappers ~group ["in"] []
+    "Input files and directories; order matters"
+  in
+  let out_cp = new CF.list_cp CF.string_wrappers ~group ["out"] []
+    "Output files and directories; order matters"
+  in
+  let chains_cp = new CF.list_cp CF.string_wrappers ~group ["chains"] []
+    "The chains to apply"
+  in
+  { group ; in_cp ; out_cp ; chains_cp }
+;;
+
+let load dir =
+  let file = Filename.concat dir input_basename in
+  let g = mk_spec_group () in
+  let on_type_error cp _ _ _ =
+     let msg = Printf.sprintf "Bad value type for field %s" 
+       (String.concat "." cp#get_name)
+    in
+     error ~file msg
+  in
+  try
+    g.group#read ~no_default: true ~on_type_error file;
+    { dir = dir ;
+      in_files = g.in_cp#get ;
+      out_files = g.out_cp#get ;
+      chains = g.chains_cp#get ;
+    }
+  with
+    CF.Missing_cp cp ->
+      let msg = Printf.sprintf "Missing field in %s"
+        (String.concat "." cp#get_name)
+      in
+      error ~file msg
+;;
+
+
+  
