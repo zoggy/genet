bba83a4ee3be00d189636c15d0f7bb1996353f33
zoggy <zoggy@bat8.org>
2012-06-19 08:35:05 +0200

    copy test input

--- draft/launch_server.sh
+++ draft/launch_server.sh
@@ -1,2 +1,12 @@
 export LD_LIBRARY_PATH=/opt/raptor/lib:/opt/librdf/lib:$LD_LIBRARY_PATH
-(rm -fr genet ; mkdir genet ; cd genet ; ~/devel/genet/src/genet init-dir . ; cp ~/devel/genet/src/config.txt . ; cp ~/devel/genet/draft/*.gnt in/chains/ ; (cd in/ ; git init . ; cd chains ; git add *.gnt ; git commit -am"test" ); ~/devel/genet/src/genet-rest)
+( \
+rm -fr genet ; \
+mkdir genet ; \
+cd genet ; \
+~/devel/genet/src/genet init-dir . ; \
+cp ~/devel/genet/src/config.txt . ; \
+mkdir in/data/test1 ;
+cp ~/devel/genet/draft/spec.in in/data/test1/ ; \
+cp ~/devel/genet/draft/*.gnt in/chains/ ; \
+(cd in/ ; git init . ; cd chains ; git add *.gnt ; git commit -am"test" ); \
+~/devel/genet/src/genet-rest )
