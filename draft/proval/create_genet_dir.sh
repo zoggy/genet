25ebff6668e09f74e10ef74ff3a982f4dea02b7d
zoggy <zoggy@bat8.org>
2012-08-27 16:38:19 +0200

    fix example

--- draft/proval/create_genet_dir.sh
+++ draft/proval/create_genet_dir.sh
@@ -7,7 +7,7 @@ mkdir ${DIR}
 cp -R test1 test2 ${DIR}/in/data/
 cp chains/*.gnt ${DIR}/in/chains/
 (cd ${DIR}/in/ ; \
-  git init . ; (cd git add chains/*.gnt data/*/spec.in ; git commit -am"test" ) ; \
+  git init . ; (git add chains/*.gnt data/*/spec.in ; git commit -am"test" ) ; \
   for i in "data/test1/same_fringe.mlw data/test2/*why" ; do touch $i; git add $i; git commit -am"add" ; done; \
 )
 
