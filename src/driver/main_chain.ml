b9a4f0d572a03408e94719a410d47b33b82b1f02
zoggy <zoggy@bat8.org>
2013-02-25 11:31:32 +0100

    genet-chain is replaced by subcommands chain, flatten      and flatten-all of genet

--- src/driver/main_chain.ml
+++ src/driver/main_chain.ml
@@ -23,9 +23,15 @@
 (*                                                                               *)
 (*********************************************************************************)
 
-(** Main module of the [genet-chain] tool. *)
+(** Operation on chains in [genet] tool. *)
 
+open Chn_types;;
 open Chn_ast;;
+open Cmdline;;
+
+let gendot = ref (None : string option);;
+let option_dot =
+  ("--dot", Arg.String (fun file -> gendot := Some file), "<prefix> generate dot file(s) using <prefix>");;
 
 let gen_chain_dot file chain =
   let dot = (new Chn_ast.chain_dot_printer)#dot_of_chain chain in
@@ -37,14 +43,12 @@ let gen_fchain_dot ctx file fchain =
   Misc.file_of_string ~file: file dot
 ;;
 
-
-
-let test_file ?dot file =
+let test_file file =
   try
     let cmod = Chn_io.chn_module_of_file file in
     Chn_io.print_chn_module stdout cmod;
     begin
-      match dot with
+      match !gendot with
         None -> ()
       | Some prefix ->
           let f chn =
@@ -56,79 +60,20 @@ let test_file ?dot file =
             gen_chain_dot outfile chn
           in
           List.iter f cmod.cmod_chains
-    end;
-    true
+    end
   with
     Loc.Problem pb ->
-      prerr_endline (Loc.string_of_problem pb);
-      false
+      failwith (Loc.string_of_problem pb)
 ;;
 
-let flatten opts ?dot acc s =
-  try
-    let config = Config.read_config opts.Options.config_file in
-    let rdf_wld = Grdf_init.open_graph config in
-    let ctx = { Chn_types.ctx_rdf = rdf_wld ; ctx_cfg = config ; ctx_user = None } in
-    let fullname = Chn_types.chain_name_of_string s in
-    let uri = Chn_flat.flatten ctx fullname in
-    print_endline (Printf.sprintf "%s => %s" s (Rdf_uri.string uri));
-    begin match dot with None -> () | Some file -> gen_fchain_dot ctx file uri end;
-    acc
-  with
-    e ->
-      let msg =
-        match e with
-          Failure s | Sys_error s ->  s
-        | Loc.Problem pb -> Loc.string_of_problem pb
-        | e -> Printexc.to_string e
-      in
-      let msg = Printf.sprintf "flatten %s: %s" s msg in
-      prerr_endline msg;
-      acc + 1
-;;
-
-let flatten_all opts acc =
-  try
-    let config = Config.read_config opts.Options.config_file in
-    let (chain_mods, _) = Chn_io.load_chain_files config in
-    let rdf_wld = Grdf_init.open_graph config in
-    let ctx = { Chn_types.ctx_rdf = rdf_wld ; ctx_cfg = config ; ctx_user = None } in
-    let f_chain mod_name  acc chain =
-      let fullname = Chn_types.mk_chain_name mod_name chain.chn_name in
-      try
-        let uri = Chn_flat.flatten ctx fullname in
-        print_endline (Printf.sprintf "%s => %s"
-          (Chn_types.string_of_chain_name fullname) (Rdf_uri.string uri));
-        acc
-      with
-        e ->
-          let msg =
-            match e with
-              Failure s | Sys_error s ->  s
-            | Loc.Problem pb -> Loc.string_of_problem pb
-            | e -> Printexc.to_string e
-          in
-          let msg = Printf.sprintf "flatten %s: %s"
-            (Chn_types.string_of_chain_name fullname) msg in
-          prerr_endline msg;
-          acc + 1
-    in
-    let f_mod acc cmod =
-      List.fold_left (f_chain cmod.cmod_name) acc cmod.cmod_chains
-    in
-    List.fold_left f_mod acc chain_mods
-  with
-    e ->
-      let msg =
-        match e with
-          Failure s | Sys_error s ->  s
-        | Loc.Problem pb -> Loc.string_of_problem pb
-        | e -> Printexc.to_string e
-      in
-      let msg = Printf.sprintf "flatten-all: %s" msg in
-      prerr_endline msg;
-      acc + 1
-;;
+let com_test _ _ opts =
+  List.iter test_file opts.Options.args;;
+let com_test = {
+    com_options = [ option_dot ] ;
+    com_usage = "<chain file(s)>" ;
+    com_kind = Main_cmd.mk_final_fun com_test ;
+  };;
+let com_test = ("test", com_test, "test the given chain files");;
 
 let string_of_comb ctx comb =
   let f_version tool version acc =
@@ -141,55 +86,109 @@ let string_of_comb ctx comb =
   String.concat ", " (Urimap.fold f_version comb [])
 ;;
 
-let show_combinations opts acc s =
-  let uri = Rdf_uri.uri s in
-  let config = Config.read_config opts.Options.config_file in
-  let rdf_wld = Grdf_init.open_graph config in
-  let ctx = { Chn_types.ctx_rdf = rdf_wld ; ctx_cfg = config ; ctx_user = None } in
-  let combs = Chn_inst.version_combinations ctx uri in
-  print_endline (Printf.sprintf "combinations for %s:" (Rdf_uri.string uri));
-  let f_comb comb = print_endline (string_of_comb ctx comb) in
-  List.iter f_comb combs;
-  acc
+let show_combinations ctx opts =
+  match opts.Options.args with
+    [s] ->
+      let uri = Rdf_uri.uri s in
+      let combs = Chn_inst.version_combinations ctx uri in
+      print_endline (Printf.sprintf "combinations for %s:" (Rdf_uri.string uri));
+      let f_comb comb = print_endline (string_of_comb ctx comb) in
+      List.iter f_comb combs
+  | _ ->
+      failwith "Please give one and only one flat chain uri"
 ;;
-
-let dot = ref None;;
-type action =
-  | Test of string
-  | Flatten of string
-  | Flatten_all
-  | Show_combs of string
-;;
-let actions = ref [];;
-
-let do_action opts acc = function
-  Test file -> if test_file ?dot: !dot file then acc else acc + 1
-| Flatten s -> flatten opts ?dot: !dot acc s
-| Flatten_all -> flatten_all opts acc
-| Show_combs s -> show_combinations opts acc s
-;;
-
-let options =
-  Options.option_config ::
-  [
-    "--dot", Arg.String (fun file -> dot := Some file), " generate dot files" ;
-    "-t", Arg.String (fun s -> actions := Test s :: !actions), "<file> test the given file" ;
-    "-f", Arg.String (fun s -> actions := Flatten s :: !actions), "<Mod.chain> flatten the given chain" ;
-    "--flatten-all", Arg.Unit (fun s -> actions := Flatten_all :: !actions), "flatten all the chains" ;
-
-    "--show-combs", Arg.String (fun s -> actions := Show_combs s :: !actions),
-    "<flat chain uri> show tool combinations usable to instanciate this chain";
-  ]
+let com_showcombs = {
+    com_options = [] ;
+    com_usage = "<flat chain uri>" ;
+    com_kind = Main_cmd.mk_final_fun (Main_cmd.mk_ctx_fun show_combinations) ;
+  };;
+let com_showcombs =
+  ("tool-combs", com_showcombs,
+   "show tool combinations usable to instanciate a flat chain")
 ;;
 
 
-let main () =
-  let opts = Options.parse options in
-  let n = List.fold_left (do_action opts) 0 (List.rev !actions) in
-  flush stdout;
-  if n > 0 then
-    prerr_endline (Printf.sprintf "%d problem(s) encountered" n);
-  exit n
+let command_chain = {
+  com_options = [] ;
+  com_usage = "" ;
+  com_kind = Cmdline.Commands
+      [
+        com_test ;
+        com_showcombs ;
+      ]
+};;
+
+let flatten ctx opts =
+  match opts.Options.args with
+    [s] ->
+      begin
+        try let fullname = Chn_types.chain_name_of_string s in
+          let uri = Chn_flat.flatten ctx fullname in
+          print_endline (Printf.sprintf "%s => %s" s (Rdf_uri.string uri));
+          begin
+            match !gendot with
+              None -> ()
+            | Some file -> gen_fchain_dot ctx file uri
+          end
+        with
+          e ->
+            let msg =
+              match e with
+                Failure s | Sys_error s ->  s
+              | Loc.Problem pb -> Loc.string_of_problem pb
+              | e -> Printexc.to_string e
+            in
+            let msg = Printf.sprintf "flatten %s: %s" s msg in
+            failwith msg
+      end
+  | _ ->
+      failwith "Please give one and only one chain fullname"
 ;;
 
-let () = Misc.safe_main main;;
+let com_flatten = {
+    com_options = [ option_dot ] ;
+    com_usage = "<Mod.chain>" ;
+    com_kind = Main_cmd.mk_final_fun (Main_cmd.mk_ctx_fun flatten) ;
+  };;
+
+let flatten_all ctx opts =
+  let (chain_mods, _) = Chn_io.load_chain_files ctx.ctx_cfg in
+  let f_chain mod_name acc chain =
+    let fullname = Chn_types.mk_chain_name mod_name chain.chn_name in
+    try
+      let uri = Chn_flat.flatten ctx fullname in
+      print_endline (Printf.sprintf "%s => %s"
+       (Chn_types.string_of_chain_name fullname) (Rdf_uri.string uri));
+      acc
+    with
+      e ->
+        let msg =
+          match e with
+            Failure s | Sys_error s ->  s
+          | Loc.Problem pb -> Loc.string_of_problem pb
+          | e -> Printexc.to_string e
+        in
+        let msg = Printf.sprintf "flatten %s: %s"
+          (Chn_types.string_of_chain_name fullname) msg
+        in
+        msg :: acc
+  in
+  let f_mod acc cmod =
+    List.fold_left (f_chain cmod.cmod_name) acc cmod.cmod_chains
+  in
+  match List.fold_left f_mod [] chain_mods with
+    [] -> ()
+  | errors -> failwith (String.concat "\n" errors)
+;;
+
+let com_flatten_all = {
+    com_options = [ ] ;
+    com_usage = "" ;
+    com_kind = Main_cmd.mk_final_fun (Main_cmd.mk_ctx_fun flatten_all) ;
+  };;
+
+Main_cmd.register_subcommand "chain" command_chain "operations on chains";;
+Main_cmd.register_subcommand "flatten" com_flatten "flatten a chain";;
+Main_cmd.register_subcommand "flatten-all" com_flatten_all "flatten all chains";;
+
+
