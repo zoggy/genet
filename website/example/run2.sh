ebbcb1186571b7c82eb832dc0fa3c926144f2ae3
zoggy <zoggy@bat8.org>
2013-03-12 09:16:32 +0100

    continue example

--- website/example/run2.sh
+++ website/example/run2.sh
@@ -19,7 +19,8 @@ genet add version http://localhost:8082/tools/split-text/branches/0.x 0.2
 #id=addsplittextintf
 INTF=`genet add interface -p "/tmp/tools/split-text-%v -p" \
   http://localhost:8082/tools/split-text/branches/0.x split-in-pars`
-  # http://localhost:8082/tools/split-text/interfaces/split-in-pars
+
+# http://localhost:8082/tools/split-text/interfaces/split-in-pars
 
 #id=addsplittextin
 genet add port ${INTF} "in" "text"
@@ -37,26 +38,22 @@ genet add version http://localhost:8082/tools/average/branches/0.x  0.3
   # http://localhost:8082/tools/average/versions/0.3
 
 #id=addaverage-c
-INTF=`genet add interface -p "/tmp/tools/average-%v -c" \
+INTFC=`genet add interface -p "/tmp/tools/average-%v -c" \
   http://localhost:8082/tools/average/branches/0.x text-length`
-  # http://localhost:8082/tools/average/interfaces/text-length
-genet add port ${INTF} "in" "text"
-genet add port ${INTF} "out" "number"
-
-#id=addnointfaverage-c
-genet add no-interface http://localhost:8082/tools/average/versions/0.1 ${INTF}
-genet add no-interface http://localhost:8082/tools/average/versions/0.2 ${INTF}
+genet add port ${INTFC} "in" "text"
+genet add port ${INTFC} "out" "number"
 
 #id=addaverage-n
-INTF=`genet add interface -p "/tmp/tools/average-%v -n" \
+INTFN=`genet add interface -p "/tmp/tools/average-%v -n" \
   http://localhost:8082/tools/average/branches/0.x average-of-set`
-  # http://localhost:8082/tools/average/interfaces/average-of-set
-genet add port ${INTF} "in" "number set"
-genet add port ${INTF} "out" "number"
+genet add port ${INTFN} "in" "number set"
+genet add port ${INTFN} "out" "number"
 
-#id=addnointfaverage-n
-genet add no-interface http://localhost:8082/tools/average/versions/0.1 ${INTF}
-genet add no-interface http://localhost:8082/tools/average/versions/0.2 ${INTF}
+#id=addnointfaverage
+genet add no-interface http://localhost:8082/tools/average/versions/0.1 ${INTFC}
+genet add no-interface http://localhost:8082/tools/average/versions/0.2 ${INTFC}
+genet add no-interface http://localhost:8082/tools/average/versions/0.1 ${INTFN}
+genet add no-interface http://localhost:8082/tools/average/versions/0.2 ${INTFN}
 
 #id=flattenall
 genet flatten-all
